2c96807005bbca85b39cc901e4e676c117a1ca20
charverr <xverropoulos@gmail.com>
2024-02-23 00:54:22 +0200

    display and delete images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,24 +3,23 @@ import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
-
+import ImageCard from './components/ImageCard';
+import { Container, Row, Col } from 'react-bootstrap';
 const USPLASH_KEY = process.env.REACT_APP_USPLASH_KEY;
 
 const App = () => {
   const [word, setWord] = useState('');
 
   const [images, setImages] = useState([]);
-  console.log(images);
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
-    console.log(word);
     fetch(
       `https://api.unsplash.com/photos/random/?query=${word}&client_id=${USPLASH_KEY}`,
     )
       .then((res) => res.json)
       .then((data) => {
-        setImages([data, ...images]); //use JavaScript spread operator in order to pull elements from particullar array
+        setImages([{ ...data,title:word}, ...images]); //use JavaScript spread operator in order to pull elements from particullar array
       })
       .catch((err) => {
         console.log(err);
@@ -28,12 +27,24 @@ const App = () => {
     setWord('');
   };
 
-  console.log(process.env.REACT_APP_USPLASH_KEY);
+  const HandleDeleteImage = (id) =>{
+    setImages(images.filter((image) => image.id !==id));
+  };
+
 
   return (
     <div>
       <Header title="Images Gallery 2" />
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+      <Container>
+        <Row xs={1} md={2} lg={3}>
+          {images.map((image, i)=>(
+            <Col key={i} classname="pb-3">
+              <ImageCard image={image} deleteImage={HandleDeleteImage} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 };
---
+++ frontend/src/components/ImageCard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Card, Button } from 'react-bootstrap';
+import PropTypes from 'prop-types'; // Import PropTypes
+
+
+const ImageCard = ({ image, deleteImage }) =>{
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={image?.urls?.small} />
+      <Card.Body>
+        <Card.Title>{image?.title.toUpperCase()}</Card.Title>
+        <Card.Text>
+          {image?.description || image?.alt_description}
+        </Card.Text>
+        <Button variant="primary" onClick={() => deleteImage(image.id)}>
+          Delete
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+};
+// Define PropTypes for the ImageCard component
+ImageCard.propTypes = {
+  image: PropTypes.object.isRequired, // Prop validation for the image object
+  deleteImage: PropTypes.func.isRequired // Prop validation for the deleteImage function
+};
+
+
+export default ImageCard;
