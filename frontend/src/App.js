17f2b296fe5995480b0d1efb9c9dbc3a5ae875e3
charverr <xverropoulos@gmail.com>
2024-02-22 12:20:20 +0200

    fixed code with linter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,37 @@
 import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header'; 
-import Search from './components/Search'
+import Header from './components/Header';
+import Search from './components/Search';
 
-const USPLASH_KEY=process.env.REACT_APP_USPLASH_KEY;
+const USPLASH_KEY = process.env.REACT_APP_USPLASH_KEY;
 
 const App = () => {
-  const [word, setWord] = useState('')
+  const [word, setWord] = useState('');
 
-  const handleSearchSubmit = (e) =>{
+  const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${USPLASH_KEY}`)
-     .then((res) => res.json)
-     .then((data) =>{
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${USPLASH_KEY}`,
+    )
+      .then((res) => res.json)
+      .then((data) => {
         console.log(data);
-     })
-     .catch((err) =>{
+      })
+      .catch((err) => {
         console.log(err);
-     })
+      });
     setWord('');
-  }
+  };
 
   console.log(process.env.REACT_APP_USPLASH_KEY);
 
   return (
     <div>
-      <Header title="Images Gallery 2"/>
-      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
+      <Header title="Images Gallery 2" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
-}
+};
 
 export default App;
